23ce4f49b2189d27270adeb2c32cb68486d9a26f
Chaitanya-Chaurasia <98615500+Chaitanya-Chaurasia@users.noreply.github.com>
2023-12-05 22:12:14 -0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-// import React from "react";
-// import { Container, Row, Col } from "react-bootstrap";
-// import Header from "./components/Header.jsx";
-// import Footer from "./components/Footer";
-// import HomePage from "./Pages/HomePage";
-// import LandingPage from "./Pages/LandingPage";
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Header from "../src/Components/Header";
+import Footer from "../src/components/Footer";
+import HomePage from "../src/Pages/HomePage";
 import backgroundImage from "../src/assets/bg.jpg"; // Update the path
 
 function App() {
@@ -15,7 +14,7 @@ function App() {
 
   return (
     <div className="d-flex flex-column min-vh-100" style={containerStyle}>
-      {/* <Header />
+      <Header />
       <Container className="flex-grow-1">
         <Row>
           <Col>
@@ -23,7 +22,7 @@ function App() {
           </Col>
         </Row>
       </Container>
-      <Footer /> */}
+      <Footer />
     </div>
   );
 }
